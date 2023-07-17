63a3f7912bed3520fcc5942de60440b67947e8e4
arnaud rigourd <arnaud.rigourd@hotmail.fr>
2023-07-17 18:23:36 +0200

    Add history memory to allow user to chat like with chatGPT

--- vocalAssistant/main.py
+++ vocalAssistant/main.py
@@ -1,9 +1,12 @@
 import logging
 import os
+from functools import lru_cache
+from pathlib import Path
 
 import openai
 from dotenv import load_dotenv
-from elevenlabs import generate
+from elevenlabs import generate, play
+from tinydb import TinyDB
 
 load_dotenv()
 
@@ -14,15 +17,23 @@ logging.basicConfig(
     format="%(asctime)s - %(levelname)s : %(message)s",
 )
 
+CUR_DIR = Path(__file__).resolve().parent
+db = TinyDB(f"{CUR_DIR}/history.json", indent=4)
+chats = db.table("Chats")
 
-def ask_chatbot(instructions):
+
+def ask_chatbot(instructions) -> None:
+    print("Thinking...")
     gpt_answer = interact_with_GPT(instructions)
-    # audio = _generate_audio_from_text(gpt_answer[6:])
-    # play(audio)
-    return gpt_answer
+    logging.debug("interact_with_GPT: success")
+    audio = _generate_audio_from_text(gpt_answer[6:])
+    logging.debug("_generate_audio_from_text: success")
+    print(gpt_answer)
+    return play(audio)
 
 
-def _setup_initial_GPT_prompt() -> str:
+@lru_cache
+def _get_initial_GPT_prompt() -> str:
     """Set up the intial GPT prompt"""
     initial_prompt = "Tu es un chatbot dont le rôle est de m'assister dans mes problématiques quotidiennes. Tu es notamment spécialisé sur le langage Python depuis plus de 20 ans et tu as une connaissance parfaite de toutes les librairies, modules et packages permettant d'optimiser les fonctions que tu me proposes pour répondre à mes demandes. Tu agis en tant que consultant perfectionniste et a pour but de toujours apporter la réponse la plus précise. Pour atteindre cet objectif, tu n'hésites pas à me poser des questions si ma demande n'est pas assez claire pour que tu puisses y répondre parfaitement. Je vais t'envoyer des prompts en français, je veux que tu me répondes EN ANGLAIS à chaque fois. Ne réponds pas à ce message qui a pour but de t'indiquer ton role. Réponds à partir du prochain prompt qui te sera envoyé. Tes réponse ne doivent êtres concises et ne doivent ABSOLUMENT PAS dépasser les 10000 caractères."
 
@@ -32,19 +43,26 @@ def _setup_initial_GPT_prompt() -> str:
 def interact_with_GPT(instructions: str = None) -> str:
     """Send the use prompt to GPT3.5-turbo API and return the response"""
     openai.api_key = os.getenv("OPENAI_API_KEY")
-    initial_prompt = _setup_initial_GPT_prompt()
+    initial_prompt = _get_initial_GPT_prompt()
+
+    if chats:
+        chats.insert_multiple([
+            {"role": "user", "content": instructions},
+        ])
+    else:
+        chats.insert_multiple([
+            {"role": "system", "content": initial_prompt},
+            {"role": "user", "content": instructions},
+        ])
+
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": initial_prompt},
-            {"role": "user", "content": instructions}
-        ],
+        messages=chats.all(),
         temperature=1
     )
 
     reply = response['choices'][0]['message']['content']
-
-    # store instructions and reply in a JSON file
+    chats.insert({"role": "assistant", "content": reply})
 
     return f"GPT : {reply}"
 
@@ -73,4 +91,4 @@ if __name__ == '__main__':
         print(response)
         instructions = input("Parlez : ")
 
-#     clear JSON file eventually
+    chats.truncate()
