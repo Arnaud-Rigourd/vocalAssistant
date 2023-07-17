07daf0e492191850c767deae03c83eb28093c463
arnaud rigourd <arnaud.rigourd@hotmail.fr>
2023-07-17 15:13:11 +0200

    set up gpt API

--- vocalAssistant/main.py
+++ vocalAssistant/main.py
@@ -1,6 +1,7 @@
 import logging
 import os
 
+import openai as openai
 from dotenv import load_dotenv
 from elevenlabs import generate, play
 
@@ -16,41 +17,55 @@ logging.basicConfig(
 
 def ask_chatbot():
     instructions = input("Parlez : ")
-    GPT_prompt = _setup_GPT_prompt(instructions)
-    GPTanswer = _get_GPT_answer(GPT_prompt)
+    initial_prompt = _setup_initial_GPT_prompt()
+    GPTanswer = interact_with_GPT(initial_prompt, instructions)
     audio = _generate_audio_from_text(GPTanswer)
     return play(audio)
 
 
-def _get_user_instructions():
-    instructions = input("Parlez : ")
-    print(instructions)
-
-
-def _setup_GPT_prompt():
+def _setup_initial_GPT_prompt() -> str:
+    """Set up the intial GPT prompt"""
     initial_prompt = "Tu es un chatbot dont le rôle est de m'assister dans mes problématiques quotidiennes. Tu es notamment spécialisé sur le langage Python depuis plus de 20 ans et tu as une connaissance parfaite de toutes les librairies, modules et packages permettant d'optimiser les fonctions que tu me proposes pour répondre à mes demandes. Tu agis en tant que consultant perfectionniste et a pour but de toujours apporter la réponse la plus précise. Pour atteindre cet objectif, tu n'hésites pas à me poser des questions si ma demande n'est pas assez claire pour que tu puisses y répondre parfaitement. Je vais t'envoyer des prompts en français, je veux que tu me répondes EN ANGLAIS à chaque fois. Ne réponds pas à ce message qui a pour but de t'indiquer ton role. Réponds à partir du prochain prompt qui te sera envoyé. Tes réponse ne doivent êtres concises et ne doivent ABSOLUMENT PAS dépasser les 10000 caractères."
-    return
+
+    return initial_prompt
 
 
-def _get_GPT_answer(GPT_prompt: str) -> bool:
+def interact_with_GPT(instructions: str = None) -> str:
     """Send the use prompt to GPT4 API and return a boolean to indicate if the API request worked"""
-    pass
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    initial_prompt = _setup_initial_GPT_prompt()
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": initial_prompt},
+            {"role": "user", "content": instructions}
+        ],
+        temperature=1
+    )
+
+    reply = response['choices'][0]['message']['content']
+
+    return reply
 
 
 def _generate_audio_from_text(GPTanswer: str) -> bytes:
-    audio = generate(
-        text=GPTanswer,
-        api_key=os.environ.get('ELEVEN_API_KEY', 'default_value'),
-        voice="Elli",
-        model="eleven_monolingual_v1"
-    )
     try:
+        audio = generate(
+            text=GPTanswer,
+            api_key=os.getenv('ELEVEN_API_KEY'),
+            voice="Elli",
+            model="eleven_monolingual_v1"
+        )
+
         logging.debug("_generate_audio_from_text: call to Eleven Labs API passed")
-        return audio
+
     except:
         logging.error("Call to Eleven Labs API failed")
 
+    return audio
+
 
 if __name__ == '__main__':
-    audio = _generate_audio_from_text("hey")
-    print(type(audio))
+    prompt = "Say that everything works folks !"
+    gpt_answer = interact_with_GPT(prompt)
+    print(gpt_answer)
